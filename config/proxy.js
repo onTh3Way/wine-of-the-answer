094c1a21d3d0faaffe3bc35f7c001591987081c8
phantom <w278206624@gmail.com>
2018-07-06 11:42:54 +0800

    添加微信js文件

--- config/proxy.js
+++ config/proxy.js
@@ -2,10 +2,10 @@ module.exports = {
   '/': {
     target: 'http://120.79.60.153',
     bypass (req) {
-      if (req.url.includes('.')) {
+      const regex = /\.\D*$/
+      if (regex.test(req.url)) {
         return req.url
       }
-    },
-    headers: {host: 'http://localhost:3000'}
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -9,27 +9,3 @@ new Vue({
   el: '#root',
   render: h => h(App)
 })
-
-// wx.ready(() => {
-//   console.log('ok')
-// })
-//
-// wx.error((err) => {
-//   console.dir(err)
-// })
-//
-// axios
-//   .get('/config')
-//   .then(res => {
-//     const {noncestr, timestamp, signature} = res.data
-//     wx.config({
-//       debug: true,
-//       appId: 'wxfc3404099806c7e5',
-//       noncestr,
-//       timestamp,
-//       signature,
-//       jsApiList: ['showAllNonBaseMenuItem']
-//     })
-//   })
-//
-// wx.showAllNonBaseMenuItem()
